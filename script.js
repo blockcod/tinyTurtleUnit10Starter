2312de3fe1ee27da69a4c125687b465e3258151d
Josue <jescamilla@aclanyc.com>
2017-03-25 00:31:39 +0000

    hex_house

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 }
-
+function triangle(x){
+    
+    
+    right(90);
+    forward(x);
+    right(120);
+    forward(x);
+    right(120);
+    forward(x);
+    
+    
+}
 // Type your function call below
 
+//square();
 
-stamp();
+
+function house(){
+right(180);
+triangle(x);
+right(30);
+square(x);
+}
+function hexagon(x){
+    triangle(x);
+    right(90);
+    triangle(x);
+    right(90)
+    triangle(x);
+    right(90);
+    triangle(x);
+    right(90);
+    triangle(x);
+    right(90);
+    triangle(x);
+}
+function pentagon(x){
+    
+    
+    //triangle (x);
+    //left(108);
+    //triangle(x);
+    //left(72);
+    //triangle(x);
+    //left(72);
+    //triangle(x);
+}
+pentagon(50);
+
